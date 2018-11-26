355873665b491196029fb3bb7254a87b0baf93f0
Saryn Caister <thegreenmajority@gmail.com>
2018-11-25 19:34:19 -0500

    get song finished

--- liri.js
+++ liri.js
@@ -9,34 +9,42 @@ const keys = require('./keys.js');
 const spotify = new Spotify(keys.spotify);
 
 if (process.argv[2] === "spotify-this-song") {
-    var getSong = process.argv[3];
+
+        var getSong = process.argv[3];
+
+        if (getSong === undefined) {
+            getSong = "the sign ace of base";
+        }
 
     spotify.search({ type: 'track', query: getSong, limit: 1 }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
+        } else {
+            var eachResult = data.tracks.items[0];
+            var spotifySongName = eachResult.name;
+            var spotifyBandName = eachResult.artists[0].name;
+            var spotifyAlbumName = eachResult.album.name;
+            var spotifyPreviewUrl = eachResult.preview_url;
+            var spotifyLink = data.tracks.href;
+
+            console.log('Song Name: ' + spotifySongName);
+            console.log('Band Name: ' + spotifyBandName);
+            console.log('Album Name: ' + spotifyAlbumName);
+            console.log('Preview URL: ' + spotifyPreviewUrl);
+            console.log('Link to Song: ' + spotifyLink);
         }
-        
-        var spotifySongName = data.tracks.items[0].name;
 
-        var spotifyBandName = data.tracks.items[0].album.artists[0].name;
-
-        var spotifyLink = data.tracks.href.toString();
-
-        var spotifyArtistsObj = data.tracks.items[0].album.artists;
-
-        var spotifyArtists = Object.values(spotifyArtistsObj);
-
-        console.log('Song Name: ' + spotifySongName);
-        console.log('Band Name: ' + spotifyBandName);
-        console.log('Link to Song: ' + spotifyLink);
-        console.log('Artist(s): ' + Object.values(spotifyArtistsObj));
     });
-};
 
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
+} else if (process.argv[2] === "movie-this") {
+    console.log('this is a movie');
+} else if (process.argv[2] === "do-what-it-says") {
+    console.log('this is a custom command');
+} else if (process.argv[2] === "concert-this") {
+    console.log('concert listing');
+} else {
+    console.log('please enter valid command [movie-this] [do-what-it-says] [concert-this] [spotify-this-song]');
+};
 
 
 // axios
