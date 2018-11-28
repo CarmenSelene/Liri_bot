fbeb50915287cd17ad3a00dea96447e3c35b0f6a
Saryn Caister <thegreenmajority@gmail.com>
2018-11-28 11:26:31 -0500

    final

--- liri.js
+++ liri.js
@@ -10,6 +10,10 @@ const keys = require('./keys.js');
 
 const spotify = new Spotify(keys.spotify);
 
+var whatIsChoice;
+
+var moment = require('moment');
+
 if (process.argv[2] === "spotify-this-song") {
     spotifyApp();
 } else if (process.argv[2] === "do-what-it-says") {
@@ -23,16 +27,21 @@ if (process.argv[2] === "spotify-this-song") {
 };
 
 function spotifyApp(x) {
-    console.log('[Song Found]');
+    console.log('[Running spotifyApp();]');
     var getSong = process.argv[3];
 
+    if (whatIsChoice != undefined) {
+        getSong = whatIsChoice;
+    };
+
     if (getSong === undefined && x === undefined) {
+        console.log('[No Song Chosen - Submitting Default]');
         getSong = "the sign ace of base";
     } else if (getSong === undefined && x == true) {
         getSong = x;
-    }
+    };
 
-    spotify.search({ type: 'track', query: getSong, limit: 1 }, function (err, data) {
+    spotify.search({ type: 'track', query: getSong, limit: 5 }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         } else {
@@ -42,10 +51,12 @@ function spotifyApp(x) {
             var spotifyAlbumName = eachResult.album.name;
             var spotifyPreviewUrl = eachResult.preview_url;
 
+            console.log('*****');
             console.log('Song Name: ' + spotifySongName);
             console.log('Band Name: ' + spotifyBandName);
             console.log('Album Name: ' + spotifyAlbumName);
             console.log('Preview URL: ' + spotifyPreviewUrl);
+            console.log('-----');
         }
 
     });
@@ -53,38 +64,46 @@ function spotifyApp(x) {
 };
 
 function txtApp() {
-    console.log('[Command Recieved]');
+    console.log('[Running txtApp();]');
 
     fs.readFile("random.txt", "utf8", function (error, data) {
 
-        // If the code experiences any errors it will log the error to the console.
         if (error) {
             return console.log(error);
         }
 
         var dataArr = data.split(",");
-        // console.log("node liri.js " + dataArr);
         var processChoice = dataArr[0].valueOf();
-        // console.log("Run Command: " + processChoice);
-        var whatIsChoice = dataArr[1].valueOf();
-        // console.log("Submitted Input: " + whatIsChoice);
-        spotifyApp(whatIsChoice);
+        whatIsChoice = dataArr[1].valueOf();
+
+        if (processChoice == "spotify-this-song") {
+            spotifyApp();
+        };
+
+        if (processChoice === "movie-this") {
+            movieApp();
+        };
+
+        if (processChoice === "concert-this") {
+            concertApp();
+        };
 
     });
 
 };
 
 function movieApp() {
-    console.log('[Movie Found]');
+    console.log('[Running movieApp();]');
     var movieSearchTerm = process.argv[3];
 
     if (movieSearchTerm === undefined) {
+        console.log('[No Movie Chosen - Submitting Default]');
         movieSearchTerm = "Mr. Nobody";
     }
 
     axios.get("http://www.omdbapi.com/?t=" + movieSearchTerm + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
-            // console.log("response.data: " + Object.keys(response.data));
+            console.log('*****');
             console.log("Title: " + response.data.Title);
             console.log("Release Year: " + response.data.Released);
             console.log("IMDB Rating: " + response.data.imdbRating);
@@ -93,10 +112,33 @@ function movieApp() {
             console.log("Language: " + response.data.Language);
             console.log("Plot Synopsis: " + response.data.Plot);
             console.log("Actors: " + response.data.Actors);
+            console.log('-----');
         }
     );
 };
 
 function concertApp() {
-// TBC
+    console.log('[Running concertApp();]');
+    var bandSearchTerm = process.argv[3];
+
+    if (bandSearchTerm === undefined) {
+        console.log('[No Artist Chosen - Submitting Default ("John Legend")]');
+        bandSearchTerm = "John Legend";
+    }
+
+
+    axios.get("https://rest.bandsintown.com/artists/" + bandSearchTerm + "/events?app_id=56ef129237e7f760daabd22f3e92fe65&date=upcoming").then(
+        function (response) {
+            var i;
+            for (i = 0; i < response.data.length -1; i++) {
+                var inputDate = response.data[i].datetime;
+                var convertedDate = moment(inputDate).format('MM DD YYYY');
+                console.log('*****');
+                console.log("Venue Name: " + response.data[i].venue.name);
+                console.log("Event City: " + response.data[i].venue.city);
+                console.log("Event Date: " + convertedDate);
+                console.log('-----');
+            };
+        }
+    );
 };
